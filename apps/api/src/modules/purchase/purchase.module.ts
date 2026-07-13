0117842781652fd9bf9a5323eddbfaa17ade096a
cuonghx <cuong1181998@gmail.com>
2026-07-13 07:52:15 +0700

    fix(purchase): dọn ký hiệu xung đột merge sót lại trong purchase.module

--- apps/api/src/modules/purchase/purchase.module.ts
+++ apps/api/src/modules/purchase/purchase.module.ts
@@ -1,10 +1,5 @@
 import { Module } from '@nestjs/common'
-<<<<<<< Updated upstream
-=======
 import { BookLockModule } from '../book-lock/book-lock.module'
-import { PayableController } from './payable.controller'
-import { PayableService } from './payable.service'
->>>>>>> Stashed changes
 import { PurchaseReportController } from './purchase-report.controller'
 import { PurchaseReportService } from './purchase-report.service'
 import { PurchaseController } from './purchase.controller'
@@ -13,14 +8,9 @@ import { SupplierController } from './supplier.controller'
 import { SupplierService } from './supplier.service'
 
 @Module({
-<<<<<<< Updated upstream
+  imports: [BookLockModule],
   controllers: [PurchaseController, SupplierController, PurchaseReportController],
   providers: [PurchaseService, SupplierService, PurchaseReportService],
-=======
-  imports: [BookLockModule],
-  controllers: [PurchaseController, SupplierController, PurchaseReportController, PayableController],
-  providers: [PurchaseService, SupplierService, PurchaseReportService, PayableService],
->>>>>>> Stashed changes
   exports: [PurchaseService, SupplierService],
 })
 export class PurchaseModule {}
